24731a3722b6e00b74b23e74fc108b0bc210b5d2
Alex329657 <alex329657@gmail.com>
2020-01-21 01:41:33 +0200

    upd

--- lesson07/hw07-task2.py
+++ lesson07/hw07-task2.py
@@ -1,7 +1,3 @@
-"""
-Implement a function that would take the morse code as input and return a decoded human-readable string
-"""
-
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
                    'F': '..-.', 'G': '--.', 'H': '....',
@@ -19,40 +15,30 @@ MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    '(': '-.--.', ')': '-.--.-',
                    'SOS': "...−−−..."}
 
-def decodemorse(message):
-
+def decrypt(message):
     message += ' '
-
     decoded_string = ''
     morse_letter = ''
-
     for letter in message:
-
-        # checks for space
         if (letter != ' '):
-            #space counter
             i = 0
             morse_letter += letter
-
-        #if i = 1 previous character space
         elif i == 1:
             i += 1
-
         else:
             i += 1
-            # if i = 3 - new word
-
             if i == 3:
-                #space separator
                 decoded_string += ' '
-
             else:
                 decoded_string += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(morse_letter)]
                 morse_letter = ''
-
     return decoded_string
 
+def main():
+
     message = ".... . -.--   .--- ..- -.. .   ...−−−..."
-    result = decodemorse(message)
+    result = decrypt(message)
     print(result)
 
+if __name__ == '__main__':
+    main()
